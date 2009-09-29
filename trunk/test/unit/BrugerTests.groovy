c8396a47826095f506527cb698bd321a1603558b
hunterdk08@gmail.com <hunterdk08@gmail.com@a3f1fb14-ad27-11de-b87c-2b1544cb6d8d>
2009-09-29 19:09:59 +0000

    initial commit

---
+++ trunk/test/unit/BrugerTests.groovy
@@ -0,0 +1,15 @@
+import grails.test.*
+
+class BrugerTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
